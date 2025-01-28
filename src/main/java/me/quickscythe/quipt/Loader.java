9e68b94ccc9800bb67c878a6de2754f5790ed26c
QuickScythe <QuickScythe@gmail.com>
2025-01-28 01:35:46 -0600

    Implement QuiptLoader

--- src/main/java/me/quickscythe/quipt/Loader.java
+++ src/main/java/me/quickscythe/quipt/Loader.java
@@ -1,24 +1,22 @@
 package me.quickscythe.quipt;
 
 import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
-import io.papermc.paper.plugin.loader.PluginLoader;
-import io.papermc.paper.plugin.loader.library.impl.JarLibrary;
-import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver;
 import me.quickscythe.quipt.json.SimpleJsonObject;
-import me.quickscythe.quipt.utils.CoreUtils;
-import org.eclipse.aether.artifact.DefaultArtifact;
-import org.eclipse.aether.graph.Dependency;
-import org.eclipse.aether.repository.RemoteRepository;
-import org.jetbrains.annotations.NotNull;
+import me.quickscythe.quipt.utils.QuiptLoader;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Objects;
-import java.util.Scanner;
 
-public class Loader implements PluginLoader {
-    private static SimpleJsonObject loadDefaultDependencies(File dependenciesFile) throws IOException {
+public class Loader extends QuiptLoader {
+
+
+    @Override
+    public void registerDependencies(PluginClasspathBuilder classpathBuilder) {
+
+    }
+
+    @Override
+    public SimpleJsonObject defaultDependencies(File dependenciesFile) throws IOException {
         if (!dependenciesFile.exists()) dependenciesFile.createNewFile();
 
         SimpleJsonObject dependencies = new SimpleJsonObject();
@@ -58,59 +56,4 @@ public class Loader implements PluginLoader {
         return dependencies;
     }
 
-    @Override
-    public void classloader(@NotNull PluginClasspathBuilder classpathBuilder) {
-        File dependenciesFolder = new File("libraries");
-        if (!dependenciesFolder.exists()) dependenciesFolder.mkdirs();
-        for (File file : Objects.requireNonNull(dependenciesFolder.listFiles())) {
-            if (file.getName().endsWith(".jar")) {
-                classpathBuilder.addLibrary(new JarLibrary(file.toPath()));
-            }
-        }
-
-        File dependenciesFile = new File("dependencies.json");
-        SimpleJsonObject dependencies;
-        if (!dependenciesFile.exists()) {
-            try {
-                dependencies = loadDefaultDependencies(dependenciesFile);
-                try (FileWriter writer = new FileWriter(dependenciesFile)) {
-                    writer.write(dependencies.toString(2));
-                } catch (IOException e) {
-                    CoreUtils.logger().error("Loader", e);
-                    return;
-                }
-            } catch (IOException e) {
-                CoreUtils.logger().error("Loader", e);
-                return;
-            }
-        } else {
-            try {
-                Scanner scanner = new Scanner(dependenciesFile);
-                StringBuilder builder = new StringBuilder();
-                while (scanner.hasNextLine()) {
-                    builder.append(scanner.nextLine());
-                }
-                dependencies = new SimpleJsonObject(builder.toString());
-            } catch (IOException e) {
-                CoreUtils.logger().error("Loader", e);
-                return;
-            }
-        }
-
-        for (String key : dependencies.getJSONObject("repositories").keySet()) {
-            SimpleJsonObject repository = dependencies.getJSONObject("repositories").getJSONObject(key);
-            MavenLibraryResolver resolver = new MavenLibraryResolver();
-            resolver.addRepository(new RemoteRepository.Builder(
-                    key, repository.getString("type"), repository.getString("url")
-            ).build());
-            for (String depKey : repository.getJSONObject("dependencies").keySet()) {
-                SimpleJsonObject dep = repository.getJSONObject("dependencies").getJSONObject(depKey);
-                resolver.addDependency(new Dependency(
-                        new DefaultArtifact(dep.getString("group") + ":" + dep.getString("artifact") + ":" + dep.getString("version")),
-                        null
-                ));
-            }
-            classpathBuilder.addLibrary(resolver);
-        }
-    }
 }
