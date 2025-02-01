1c9aecf7f899d0675781d93785fea611fddac10d
QuickScythe <QuickScythe@gmail.com>
2025-01-31 22:52:17 -0600

    better dependency loader

--- src/main/java/me/quickscythe/quipt/Loader.java
+++ src/main/java/me/quickscythe/quipt/Loader.java
@@ -4,7 +4,6 @@ import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
 import io.papermc.paper.plugin.loader.PluginLoader;
 import io.papermc.paper.plugin.loader.library.impl.JarLibrary;
 import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver;
-import me.quickscythe.quipt.json.SimpleJsonObject;
 import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.repository.RemoteRepository;
@@ -12,10 +11,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Objects;
-import java.util.Scanner;
+import java.util.Properties;
 
 public class Loader implements PluginLoader {
 
@@ -23,98 +21,35 @@ public class Loader implements PluginLoader {
 
     @Override
     public void classloader(PluginClasspathBuilder classpathBuilder) {
-        File dependenciesFolder = new File("libraries");
+        File dependenciesFolder = new File("dependencies/quipt-paper");
         if (!dependenciesFolder.exists()) dependenciesFolder.mkdirs();
+//        File propertiesFile = new File(dependenciesFolder, "gradle.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(getClass().getResourceAsStream("/gradle.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         for (File file : Objects.requireNonNull(dependenciesFolder.listFiles())) {
             if (file.getName().endsWith(".jar")) {
                 classpathBuilder.addLibrary(new JarLibrary(file.toPath()));
             }
+
         }
-
-        File dependenciesFile = new File("dependencies-quipt-paper.json");
-        SimpleJsonObject dependencies;
-        if (!dependenciesFile.exists()) {
-            try {
-                dependencies = defaultDependencies(dependenciesFile);
-                try (FileWriter writer = new FileWriter(dependenciesFile)) {
-                    writer.write(dependencies.toString(2));
-                } catch (IOException e) {
-                    logger.error("Loader", e);
-                    return;
-                }
-            } catch (IOException e) {
-                logger.error("Loader", e);
-                return;
-            }
-        } else {
-            try {
-                Scanner scanner = new Scanner(dependenciesFile);
-                StringBuilder builder = new StringBuilder();
-                while (scanner.hasNextLine()) {
-                    builder.append(scanner.nextLine());
-                }
-                dependencies = new SimpleJsonObject(builder.toString());
-            } catch (IOException e) {
-                logger.error("Loader", e);
-                return;
-            }
-        }
+        System.out.println(properties);
+        MavenLibraryResolver central = new MavenLibraryResolver();
+        central.addRepository(new RemoteRepository.Builder("central", "default", "https://repo.maven.apache.org/maven2/").build());
+        central.addDependency(new Dependency(new DefaultArtifact("org.json:json:" + properties.getProperty("json_version")), null));
+        central.addDependency(new Dependency(new DefaultArtifact("org.eclipse.jgit:org.eclipse.jgit:" + properties.getProperty("jgit_version")), null));
 
 
-        for (String key : dependencies.getJSONObject("repositories").keySet()) {
-            SimpleJsonObject repository = dependencies.getJSONObject("repositories").getJSONObject(key);
-            MavenLibraryResolver resolver = new MavenLibraryResolver();
-            resolver.addRepository(new RemoteRepository.Builder(
-                    key, repository.getString("type"), repository.getString("url")
-            ).build());
-            for (String depKey : repository.getJSONObject("dependencies").keySet()) {
-                SimpleJsonObject dep = repository.getJSONObject("dependencies").getJSONObject(depKey);
-                resolver.addDependency(new Dependency(
-                        new DefaultArtifact(dep.getString("group") + ":" + dep.getString("artifact") + ":" + dep.getString("version")),
-                        null
-                ));
-            }
-            classpathBuilder.addLibrary(resolver);
-        }
-    }
-
-    public SimpleJsonObject defaultDependencies(File dependenciesFile) throws IOException {
-        if (!dependenciesFile.exists()) dependenciesFile.createNewFile();
-
-        SimpleJsonObject dependencies = new SimpleJsonObject();
-        SimpleJsonObject central = new SimpleJsonObject();
-        central.put("type", "default");
-        central.put("url", "https://repo1.maven.org/maven2/");
-        SimpleJsonObject jgit = new SimpleJsonObject();
-        jgit.put("group", "org.eclipse.jgit");
-        jgit.put("artifact", "org.eclipse.jgit");
-        jgit.put("version", "6.6.1.202309021850-r");
-
-        SimpleJsonObject json = new SimpleJsonObject();
-        json.put("group", "org.json");
-        json.put("artifact", "json");
-        json.put("version", "20250107");
-
-        central.put("dependencies", new SimpleJsonObject().put("jgit", jgit).put("json", json));
-
-        SimpleJsonObject quipt = new SimpleJsonObject();
-        quipt.put("type", "default");
-        quipt.put("url", "https://repo.vanillaflux.com/repository/quipt/");
-        SimpleJsonObject bot = new SimpleJsonObject();
-        bot.put("group", "me.quickscythe");
-        bot.put("artifact", "quipt-core");
-        bot.put("version", "0.1.1-alpha-B1");
-
-        SimpleJsonObject core = new SimpleJsonObject();
-        core.put("group", "me.quickscythe");
-        core.put("artifact", "quipt-paper");
-        core.put("version", "1.1.8-alpha-B5");
-
-        quipt.put("dependencies", new SimpleJsonObject().put("bot", bot).put("core", core));
-
-        dependencies.put("repositories", new SimpleJsonObject().put("central", central).put("quipt", quipt));
+        MavenLibraryResolver quipt = new MavenLibraryResolver();
+        quipt.addRepository(new RemoteRepository.Builder("quipt", "default", "https://repo.vanillaflux.com/repository/quipt/").build());
+        quipt.addDependency(new Dependency(new DefaultArtifact("me.quickscythe:quipt-bot:" + properties.getProperty("quipt_bot_version")), null));
+        quipt.addDependency(new Dependency(new DefaultArtifact("me.quickscythe:quipt-core:" + properties.getProperty("quipt_core_version")), null));
 
 
-        return dependencies;
+        classpathBuilder.addLibrary(central);
+        classpathBuilder.addLibrary(quipt);
     }
 }
