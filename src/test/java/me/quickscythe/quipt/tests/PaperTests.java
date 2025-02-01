08c203a6dfcd3594ccda1aad4a190a218491967f
QuickScythe <QuickScythe@gmail.com>
2025-02-01 03:08:42 -0600

    alwaysPass test so build succeeds

---
+++ src/test/java/me/quickscythe/quipt/tests/PaperTests.java
@@ -0,0 +1,13 @@
+package me.quickscythe.quipt.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PaperTests {
+
+    @Test
+    void alwaysPass(){
+        assertTrue(true);
+    }
+}
